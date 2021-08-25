ce4d32c8a67e2ad443d84a549c74de8f89b8ec24
Lucas de Farias Teixeira <programmer.lucas.teixeira@gmail.com>
2021-08-25 09:58:31 -0300

    Update urls.py

--- Entra21/Ecommerce/categorias/api/urls.py
+++ Entra21/Ecommerce/categorias/api/urls.py
@@ -1,3 +1,13 @@
+from categorias.api.viewsets import CategoriaViewSet, TagViewSet
 from django.urls import path
 
 app_name = 'categorias'
+
+"""
+Urls da API.
+"""
+
+urlpatterns = [
+    path('/tags', TagViewSet.list, name='taglist'),
+    path('/categorias', CategoriaViewSet, name='categorialist'),
+]
