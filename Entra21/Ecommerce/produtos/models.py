2f65c4924c2856ae44fc301b9f98d5c82b3e972f
Lucas de Farias Teixeira <programmer.lucas.teixeira@gmail.com>
2021-08-27 12:10:30 -0300

    .

--- Entra21/Ecommerce/produtos/models.py
+++ Entra21/Ecommerce/produtos/models.py
@@ -19,3 +19,12 @@ class Produto(models.Model):
 
     def __str__(self):
         return self.nome
+
+    def __eq__(self, other):
+        return self.nome == other.nome
+
+    def __gt__(self, other):
+        return self.visualizacoes > other.visualizacoes
+
+    def __lt__(self, other):
+        return self.visualizacoes < other.visualizacoes
