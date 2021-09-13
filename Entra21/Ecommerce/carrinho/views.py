85413105a44abcd4d87438db7f7009ee0194e702
PedroLuscao <pedrolucas4362@gmail.com>
2021-09-13 10:17:45 -0300

    .

--- Entra21/Ecommerce/carrinho/views.py
+++ Entra21/Ecommerce/carrinho/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from produtos.models import Produto
 import requests
 
 def retorna_carrinho(request, pk):
@@ -7,6 +8,21 @@ def retorna_carrinho(request, pk):
 
     O retorno consiste em um template acompanhado de um json.
     """
-    dados = requests.get('http://127.0.0.1:8000/api/carrinho/' + pk + '/')
+
+    # Itens carrinho
+
+    dados = requests.get('http://127.0.0.1:8000/api/carrinho/' + str(pk) + '/')
     dados = dados.json()
-    return render(request, 'carrinho/carrinho.html', {'dados':dados})
+    produtos = [get_object_or_404(Produto, id=i) for i in dados['produtos']]
+
+    # total do carrinho
+
+    itens = requests.get('http://127.0.0.1:8000/api/carrinho/' + str(pk) + '/')
+    itens = itens.json()
+    total_itens = len(list(itens))
+    prod = [get_object_or_404(Produto, id=i) for i in itens['produtos']]
+    total = [i.preco for i in prod]
+    total = sum(total)
+
+
+    return render(request, 'carrinho/carrinho.html', {'dados':produtos, 'total_itens': total_itens, 'total': total})
