e9e0cb5d69e03133f203474fbe29443d84d91e3a
Lucas de Farias Teixeira <programmer.lucas.teixeira@gmail.com>
2021-09-13 15:51:53 -0300

    Update views.py

--- Entra21/Ecommerce/carrinho/views.py
+++ Entra21/Ecommerce/carrinho/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.shortcuts import render, get_object_or_404
 from produtos.models import Produto
 import requests
 
@@ -16,13 +16,10 @@ def retorna_carrinho(request, pk):
     produtos = [get_object_or_404(Produto, id=i) for i in dados['produtos']]
 
     # total do carrinho
-
-    itens = requests.get('http://127.0.0.1:8000/api/carrinho/' + str(pk) + '/')
-    itens = itens.json()
-    total_itens = len(list(itens))
-    prod = [get_object_or_404(Produto, id=i) for i in itens['produtos']]
-    total = [i.preco for i in prod]
+    total_itens = len(list(dados))
+    total = [i.preco for i in produtos]
     total = sum(total)
 
 
     return render(request, 'carrinho/carrinho.html', {'dados':produtos, 'total_itens': total_itens, 'total': total})
+
