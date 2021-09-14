6476883a1b869829fc7e34817b1f72a841a67041
PedroLuscao <pedrolucas4362@gmail.com>
2021-09-14 11:21:46 -0300

    arrumei alguns erros

--- Entra21/Ecommerce/produtos/urls.py
+++ Entra21/Ecommerce/produtos/urls.py
@@ -9,7 +9,7 @@ URLs do app produtos.
 
 urlpatterns = [
     path('<int:pk>/', detalhes_produto, name='retrieve'),
-    path('list/', retorna_produtos, 'list'),
+    path('list/', retorna_produtos, name='list'),
     path('maisVendidos/', retorna_produtos_mais_vendidos, name='maisVendidos'),
     path('maisVistos', retorna_produtos_mais_visualizados, name='maisVistos'),
     path('lancamentos/', retorna_produtos_mais_recentes, name='lancamentos'),
