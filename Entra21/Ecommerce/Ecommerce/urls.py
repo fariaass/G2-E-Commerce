73fef00b820a18bf11b4fa1c4c4c646edb8d9f90
Pedro Felipe <pedrofmm13@gmail.com>
2021-09-17 14:02:13 -0300

    eita
    eitaeita

--- Entra21/Ecommerce/Ecommerce/urls.py
+++ Entra21/Ecommerce/Ecommerce/urls.py
@@ -22,3 +22,4 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- Entra21/Ecommerce/carrinho/urls.py
+++ Entra21/Ecommerce/carrinho/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from carrinho.views import carrinho_remove, carrinho_add, carrinho_detail
+from carrinho.views import retorna_carrinho
 
 app_name = 'carrinho'
 
@@ -8,8 +8,5 @@ URLs do app carrinho.
 """
 
 urlpatterns = [
-    # path('<int:pk>/', car, name='retrieve'),
-    path("", carrinho_detail, name="carrinho_detail"),
-    path("add/<int:product_id>/", carrinho_add, name="add"),
-    path("remove/<int:product_id>/", carrinho_remove, name="remove"),
+    path('<int:pk>/', retorna_carrinho, name='retrieve')
 ]
