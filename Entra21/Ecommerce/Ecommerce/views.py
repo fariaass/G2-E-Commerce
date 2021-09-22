b53be9aa5a2ce0f5a1a6849eabf9f827e92572be
Pedro Felipe <pedrofmm13@gmail.com>
2021-09-22 10:59:19 -0300

    ajustes
    ajustes navbar, ajuste snippets, e criação de campo de imagem na categoria

--- Entra21/Ecommerce/Ecommerce/views.py
+++ Entra21/Ecommerce/Ecommerce/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from categorias.models import Categoria
 import requests
 
 def home(request):
-    return render(request, 'index.html')
+    categorias = Categoria.objects.all()
+    return render(request, 'home.html', {'categorias': categorias})
 
