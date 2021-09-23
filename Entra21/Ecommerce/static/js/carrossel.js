08c3c7254a22a3803aa9c16d5dce4b94ccf0b6ca
Pedro Felipe <pedrofmm13@gmail.com>
2021-09-22 22:25:53 -0300

    mais coisa home

--- Entra21/Ecommerce/static/js/carrossel.js
+++ Entra21/Ecommerce/static/js/carrossel.js
@@ -10,20 +10,20 @@ new Glider($responsiveCarousel, {
     prev: ".js-carousel--responsive-prev",
     next: ".js-carousel--responsive-next",
   },
-  // responsive: [
-  //   {
-  //     breakpoint: 600,
-  //     settings: {
-  //       slidesToShow: 2,
-  //       slidesToScroll: 2,
-  //     },
-  //   },
-  //   {
-  //     breakpoint: 900,
-  //     settings: {
-  //       slidesToShow: 3,
-  //       slidesToScroll: 3,
-  //     },
-  //   },
-  // ],
+  responsive: [
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      },
+    },
+  ],
 });
