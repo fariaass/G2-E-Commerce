e1049150482975aad4e32c9835fe206a4e9228d6
adriano <adriano@professor-debian.sol.app.br>
2021-10-13 16:01:13 -0400

    modified:   Entra21/Ecommerce/Ecommerce/wsgi.py

--- Entra21/Ecommerce/Ecommerce/wsgi.py
+++ Entra21/Ecommerce/Ecommerce/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ecommerce.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Entra21.Ecommerce.Ecommerce.settings')
 
 application = get_wsgi_application()
